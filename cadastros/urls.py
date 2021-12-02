9cce3f724da14f7b4368e7ea0bb5ec3fdb074993
Vitor Edson <65077901+vitoredsonrs@users.noreply.github.com>
2021-12-02 09:08:49 -0300

    Atualizando e adicionando mais algumas funções.

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -1,10 +1,9 @@
-from django.urls import include, path
-
-from cadastros.views import lista_cidades, detalhe_cidade
+from django.urls import path
+from cadastros.views import lista_cidades, detalhe_cidade, cadastra_cidade
 
 urlpatterns = [
 
     path('', lista_cidades, name='cidades-list'),
     path('detail/<int:id>/', detalhe_cidade, name='cidades-detalhe'),
+    path('create', cadastra_cidade, name='cadastra-cidade'),
 ]
-
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
+from cadastros.forms import CidadeForm
 from cadastros.models import Cidade
 
 
 def lista_cidades(request):
     # Retornar a lista de cidades cadastradas;
     # orm do Django;
-    qs = Cidade.objects.all()
+    qs = Cidade.objects.all().order_by('nome')
 
     context = {
         'cidades': qs,
@@ -26,3 +27,21 @@ def detalhe_cidade(request, id):
     }
 
     return render(request, 'cadastros/detalhe_cidades.html', context)
+
+
+def cadastra_cidade(request):
+
+    if request.method == 'POST':
+        form = CidadeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('cidades-list')
+    else:
+        form = CidadeForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'cadastros/cadastra_cidades.html', context)
+
