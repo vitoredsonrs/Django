95640ffa36c2b42b0c76335028483ffc7c8c5112
Vitor Edson <65077901+vitoredsonrs@users.noreply.github.com>
2021-12-04 20:13:18 -0300

    Migrating DB from SQLite to Postgres.

--- proj/settings.py
+++ proj/settings.py
@@ -76,9 +76,14 @@ WSGI_APPLICATION = 'proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pessoal',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
+
 }
 
 
